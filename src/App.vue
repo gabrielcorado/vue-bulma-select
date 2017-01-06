c0ca819ad1ced1cc69282ff397e2d4987070d379
Gabriel Corado <gabrielcorado@mail.com>
2017-01-06 01:38:38 -0200

    Kickoff

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <h1>Vue Bulma Select</h1>
+    <div>
+      <bulma-select
+        label="name"
+        placeholder="Select your president"
+        v-model="selection"
+        :options="options">
+      </bulma-select>
+    </div>
+    <input v-model="selection">
+  </div>
+</template>
+
+<script>
+// Load Components
+import BulmaSelect from './components/bulma/Select'
+
+// Export main component
+export default {
+  name: 'app',
+  components: {
+    BulmaSelect
+  },
+  data () {
+    return {
+      selection: undefined,
+      options: [
+        {id: 1, name: 'George'},
+        {id: 2, name: 'Barack'},
+        {id: 3, name: 'Donald'}
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/bulma/Select.vue
@@ -0,0 +1,183 @@
+<template>
+  <div :class="containerClasses" @blur="close">
+    <div :class="placeholderClasses" @click.self="toggleOpened">
+      <span v-if="selected === undefined">{{ placeholder }}</span>
+      <span v-else>{{ textSelected }}</span>
+    </div>
+    <transition name="slide-fade">
+      <div v-if="opened" :class="optionsClasses">
+        <button
+          v-for="(option, i) in options"
+          @click="selectOption(i)"
+          :class="['button is-white', selected === i ? 'is-info' : '', 'bulma-select__option']">
+          <template v-if="label !== undefined">{{ option[label] }}</template>
+          <template v-else>{{ option }}</template>
+        </button>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+// Export the component itself
+export default {
+  name: 'bulma-select',
+  props: {
+    // Select placeholder
+    placeholder: {
+      type: String,
+      default: 'Select...'
+    },
+    // Options
+    options: {
+      type: Array,
+      default: () => []
+    },
+    // Options label
+    label: {
+      type: String
+    },
+    // v-model value
+    value: {
+      type: null,
+      default: null
+    }
+  },
+  // Component inner state
+  data () {
+    //
+    return {
+      opened: false,
+      selected: undefined
+    }
+  },
+  // Computed properties
+  computed: {
+    // Container computed classes
+    containerClasses () {
+      return ['select bulma-select__container', {'bulma-select__container--active': this.opened}]
+    },
+    // Placeholder computed classes
+    placeholderClasses () {
+      return ['bulma-select__placeholder button is-white', {'is-hovered': this.opened}]
+    },
+    // Options computed classes
+    optionsClasses () {
+      return [{'is-hovered': this.opened}, 'bulma-select__options']
+    },
+    // Selected item
+    selectedItem () { return this.options[this.selected] },
+    // Text for the selected option
+    textSelected () {
+      return this.label !== undefined ? this.selectedItem[this.label] : this.selectedItem
+    }
+  },
+  // Methods
+  methods: {
+    // Toggle the opened property
+    toggleOpened () {
+      this.opened ? this.close() : this.open()
+    },
+    // Open the select
+    open () {
+      // Primitive return
+      if (this.opened) { return }
+
+      // Definitions
+      this.opened = true
+      this.$el.focus()
+    },
+    // Close the select
+    close () {
+      // Primitive return
+      if (!this.opened) { return }
+
+      // Definitions
+      this.opened = false
+      this.$el.blur()
+    },
+    // Set the selected using v-model value
+    selectVModel () {
+      if (this.value === undefined) { return }
+      this.selected = this.options.indexOf(this.value)
+    },
+    // Select a value based on the index
+    selectOption (i) {
+      // Select the item
+      this.selected = i
+
+      // Close the select
+      this.close()
+    }
+  },
+  // Watchers
+  watch: {
+    // Change the selected item
+    selected () {
+      // Emit!
+      this.$emit('input', this.options[this.selected])
+    },
+    // Value changed
+    value () {
+      this.selectVModel()
+    }
+  },
+  // When the component is created
+  created () {
+    // do it
+    // this.selectVModel()
+  }
+}
+</script>
+
+<style lang="sass">
+// Import bulma stuff
+@import '~bulma/sass/utilities/_all'
+@import "~bulma/sass/elements/button"
+@import "~bulma/sass/elements/form"
+
+//
+.bulma-select
+  &__container
+    position: relative
+    width: 100%
+    &:after
+      transition: all 0.2s ease
+    &--active
+      &:after
+        transform: rotate(-225deg)
+        top: 60% !important
+  &__placeholder
+    @extend select
+    box-sizing: border-box
+    text-align: left
+    padding-top: 6px
+    &.is-hovered
+      border-radius: $input-radius $input-radius 0 0
+  &__options
+    +input
+    display: block
+    padding: 0
+    z-index: 5
+    width: 100%
+    height: auto
+    box-sizing: border-box
+    border-top: 0
+    border-radius: 0 0 $input-radius $input-radius
+    position: absolute
+  &__option
+    width: 100%
+    text-align: left
+    border-radius: 0
+    display: block
+
+// Transition definition
+.slide-fade
+  &-enter-active
+    transition: all 0.2s ease
+  &-leave-active
+    transition: all 0.1s cubic-bezier(1.0, 0.5, 0.8, 1.0)
+  &-enter, &-leave-active
+    padding-top: 10px
+    opacity: 0
+</style>
